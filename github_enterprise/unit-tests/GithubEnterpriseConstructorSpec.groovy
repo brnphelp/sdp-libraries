a90dcb7c142fa6f995dcb4af6686114adc2f158b
ottoson-keegan <ottoson_keegan@bah.com>
2019-02-27 18:12:25 -0500

    wrapping up GithubEnterpriseConstructorSpec.groovy

--- github_enterprise/unit-tests/GithubEnterpriseConstructorSpec.groovy
+++ github_enterprise/unit-tests/GithubEnterpriseConstructorSpec.groovy
@@ -15,15 +15,98 @@ public class GithubEnterpriseConstructorSpec extends JenkinsPipelineSpecificatio
     explicitlyMockPipelineVariable("ScmUserRemoteConfig")
     explicitlyMockPipelineVariable("scm")
     getPipelineMock("scm.getUserRemoteConfigs")() >> { [getPipelineMock("ScmUserRemoteConfig")] }
-    getPipelineMock("ScmUserRemoteConfig.getUrl")() >> "https://github.com/boozallen/jenkins-templating-engine.git"
+    getPipelineMock("ScmUserRemoteConfig.getUrl")() >> "https://github.com/example-org/example-repo.git"
     getPipelineMock("sh")(_ as Map) >> " 1 "
   }
 
-  def "PLACEHOLDER" () {
+  def "unstash workspace before calling git commands" () {
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      1 * getPipelineMock("unstash")("workspace")
+    then:
+      _ * getPipelineMock("sh")({ try {it.script =~ /git/; } catch(any) {it =~ /git/} }) >> "1"
+      //returning the arbitrary string "1" to prevent the script from failing
+  }
 
+  def "GIT_URL env var is retreived from the scm object" () {
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      1 * getPipelineMock("scm.getUserRemoteConfigs")() >> { [getPipelineMock("ScmUserRemoteConfig")] }
+      1 * getPipelineMock("ScmUserRemoteConfig.getUrl")() >> "https://github.com/boozallen/jenkins-templating-engine.git"
+      GithubEnterpriseConstructor.getBinding().variables.env.GIT_URL == "https://github.com/boozallen/jenkins-templating-engine.git"
+  }
+
+  def "ORG_NAME is set to the GitHub organization in GIT_URL" () {
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      getPipelineMock("ScmUserRemoteConfig.getUrl")() >> "https://github.com/library-test/jenkins-templating-engine.git"
+      GithubEnterpriseConstructor.getBinding().variables.env.ORG_NAME == "library-test"
+  }
+
+  def "REPO_NAME is set to the GitHub repository in GIT_URL" () {
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      getPipelineMock("ScmUserRemoteConfig.getUrl")() >> "https://github.com/boozallen/library-test2.git"
+      GithubEnterpriseConstructor.getBinding().variables.env.REPO_NAME == "library-test2"
+  }
+
+  def "GIT_SHA is set to HEAD" () {
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      1 * getPipelineMock("sh")([script: "git rev-parse HEAD", returnStdout: true]) >> script_output
+      GithubEnterpriseConstructor.getBinding().variables.env.GIT_SHA == git_sha
+    where:
+      script_output   | git_sha
+      "abcd12345"     | "abcd12345"
+      "\nefgh67890\n" | "efgh67890"
+  }
+
+  def "If CHANGE_TARGET is truthy set GIT_BUILD_CAUSE to pr" () {
+    setup:
+      GithubEnterpriseConstructor.getBinding().variables.env.put("CHANGE_TARGET", change_target)
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      GithubEnterpriseConstructor.getBinding().variables.env.GIT_BUILD_CAUSE == build_cause
+    where:
+      change_target | build_cause
+      true          | "pr"
+      "true"        | "pr"
+      false         | "commit"
+      null          | "commit"
+  }
+
+  def "If the latest commit has more than 2 parents, set GIT_BUILD_CAUSE to merge, else set to commit" () {
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      1 * getPipelineMock("sh")([script: 'git rev-list HEAD --parents -1 | wc -w', returnStdout: true]) >> script_output
+      GithubEnterpriseConstructor.getBinding().variables.env.GIT_BUILD_CAUSE == build_cause
+    where:
+      script_output | build_cause
+      "1"           | "commit" // I don't anticipate script_output being less than 2
+      "2"           | "commit"
+      "3"           | "merge"
+      "4"           | "merge"
+      "  1    \n"   | "commit"
+      "\n5\n"       | "merge"
   }
 
 
+  def "The Git Build Cause is printed to the log" () {
+    when:
+      GithubEnterpriseConstructor(context)
+    then:
+      1 * getPipelineMock("echo")( _ ) >> { _arguments ->
+        def build_cause = GithubEnterpriseConstructor.getBinding().variables.env.GIT_BUILD_CAUSE
+        assert _arguments[0] == "Found Git Build Cause: ${build_cause}"
+      }
 
+  }
 
 }
