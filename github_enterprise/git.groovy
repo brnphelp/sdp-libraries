6c0755d8f921c1923caec5fe740714fb5b326549
ottoson-keegan <ottoson_keegan@bah.com>
2019-02-28 15:16:05 -0500

    adding to git.groovy's tests

--- github_enterprise/git.groovy
+++ github_enterprise/git.groovy
@@ -7,16 +7,16 @@ import org.codehaus.groovy.runtime.GStringImpl
 
 void call(Map args){
 
-  // supported git actions 
+  // supported git actions
   actions = [
     "add": { files ->
       if (files instanceof String) sh "git add ${files}"
       else if (files instanceof GStringImpl) sh "git add ${files}"
       else sh "git add ${files.join(" ")}"
-    }, 
+    },
     "commit": { message ->
       sh "git config user.email 'jenkins@nothing.com' && git config user.name 'Jenkins'"
-      sh script: "git commit -m \"${message}\"", returnStatus: true
+      sh script: "git commit -m \"${message ?: "commit from Jenkins"}\"", returnStatus: true
     },
     "push": { flags ->
       sh "git push ${flags ?: ""} ${env.git_url_with_creds}"
@@ -26,22 +26,22 @@ void call(Map args){
   // validate that there are no invalid inputs
   invalid_args = args.findAll{ !(it.getKey() in actions.keySet()) }
   if (invalid_args) error "Unknown git actions: ${invalid_args.collect{ it.getKey() }.join(", ")}"
-  
+
   // validate the user is doing an action
   if (!(args.subMap(actions.keySet()))) error "git: You must use an action: ${actions.keySet().join(", ")}"
-  
-    
+
+
   // do the things.
   args.each{ action, value ->
     def c = actions.get(action)
     c.resolveStrategy = Closure.DELEGATE_FIRST
-    c.delegate = this        
+    c.delegate = this
     c.call(value)
   }
 }
 
 void call(String action){
-  String a = action.toString()
+  String a = action
   this.call([
     (a): null
   ])
--- github_enterprise/unit-tests/GitSpec.groovy
+++ github_enterprise/unit-tests/GitSpec.groovy
@@ -9,13 +9,97 @@ public class GitSpec extends JenkinsPipelineSpecification {
   }
 
   def setup() {
-    Git = loadPipelineScriptForTest("github_enterprise/github_enterprise_constructor.groovy")
+    Git = loadPipelineScriptForTest("github_enterprise/git.groovy")
+    Git.getBinding().setVariable("env", [git_url_with_creds: "git_url_with_creds"])
   }
 
-  /** ghe() or ghe.call()  **/
-  def "PLACEHOLDER" () {
+  // call(Map args)
+  def "If inputs are invalid, throw error" () {
+    when:
+      try{Git([foo: "bar"])}catch(any){}
+    then:
+      1 * getPipelineMock("error")({ it =~ /Unknown git actions/ }) >> { throw new DummyException("invalid action")}
+  }
+
+  def "If no valid Action is given, throw an error" () {
+    when:
+      try{Git([:])}catch(any){}
+    then:
+      1 * getPipelineMock("error")({ it =~ /You must use an action\:/}) >> { throw new DummyException("no action given")}
+  }
+
+  def "Each Action in the args map is called as expected" () {
+    when:
+      Git(actions)
+    then:
+      x * getPipelineMock("sh")({it =~ /^git add files$/})
+      y * getPipelineMock("sh")({if (it instanceof Map) it.script =~ /^git commit -m \"commit from Jenkins\"$/; else false})
+      z * getPipelineMock("sh")({it =~ /^git push  git_url_with_creds/}) //note: two spaces between push and git_url
+    where:
+      actions                                  | x | y | z
+      [add: "files"]                           | 1 | 0 | 0
+      [commit: null]                           | 0 | 1 | 0
+      [push: null]                             | 0 | 0 | 1
+      [add: "files", commit: null]             | 1 | 1 | 0
+      [add: "files", push: null]               | 1 | 0 | 1
+      [commit: null, push: null]               | 0 | 1 | 1
+      [add: "files", commit: null, push: null] | 1 | 1 | 1
+  }
+
+  def "Each Action's closure's resolveStrategy is set to DELEGATE_FIRST" () {
 
   }
 
+  def "Each Action's closure's delegate is set to the current script" () {
+
+  }
+
+  // call(String action)
+  def "The call(Map args) method is called with args=[action: null]" () {
+
+  }
+
+  // "add" action
+  def "If add's argument is a String, use git add to add the file(s)" () {
+
+  }
+
+  def "If add's argument is a GStringImpl, use git add to add the file(s)" () {
+
+  }
+
+  def "If add's argument is anything else, assume it's an array or list of files, join the elements, and call git add" () {
+
+  }
+
+  // "commit" action
+  def "The git config is modified appropriately before a commit is made" () {
+
+  }
+
+  def "Git commit is used to create a commit" () {
+
+  }
+
+  def "The commit is created using the given message" () {
+
+  }
+
+  def "If the commit message is null, use a placeholder" () {
+
+  }
+
+  //"push" action
+  def "Git push is called" () {
+
+  }
+
+  def "The given flags are used when git push is called" () {
+
+  }
+
+  def "The git push command uses the URL supplied by withGit()" () {
+
+  }
 
 }
